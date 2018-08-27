bd9568f45f5c0c6d09078678712bf6cb725a5c83
Malikabhc <malika.bouhcine@gmail.com>
2018-08-27 13:57:14 +0200

    exercice2-9

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+        <meta charset="utf-8" />
+        <title>Exercice 2/9</title>
+    </head>
+    <body>
+        <p><?= //Affichage de la date au format jj-mm-aa
+        date('d-m-y'); ?></p>
+    </body>
+</html>
